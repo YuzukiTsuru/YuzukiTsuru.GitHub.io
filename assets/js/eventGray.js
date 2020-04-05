a15130ac656ff1d73dd043f5d4c95cc6da8a0362
Gloomy Ghost <740291272@qq.com>
2020-04-05 10:29:30 +0800

    Update eventGray.js

--- assets/js/eventGray.js
+++ assets/js/eventGray.js
@@ -1,7 +1,7 @@
-let date = new Date();
+let dateEOfNOW = new Date();
 let eventGrayDay = ["4.4", "9.18", "12.13"]
 
-if (eventGrayDay.indexOf((date.getMonth() + 1) + "." + (date.getDate())) !== -1) {
+if (eventGrayDay.indexOf((dateEOfNOW.getMonth() + 1) + "." + (dateEOfNOW.getDate())) !== -1) {
     $("html").css({
         "-webkit-filter": "grayscale(100%)",
         "-moz-filter": "grayscale(100%)",
